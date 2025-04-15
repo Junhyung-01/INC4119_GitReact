8cdb0177750d5e3843eb75bda5dc7485b151ff27
Junhyung-01 <chlwnsgud0326@gmail.com>
2025-04-15 14:45:27 +0900

    이미지파일 수정

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import dora from '../assets/dora.jpg';
+import choonsik from '../assets/choonsik.jpg';
 
 function ProfileCard() {
   return (
     <div>
-      <img src={dora} alt="Profile" />
-      <h2>Your Name</h2>
-      <p>Frontend Developer</p>
+      <img src={choonsik} alt="Profile" />
+      <h2>최준형</h2>
+      <p>2020112128</p>
     </div>
   );
 }
